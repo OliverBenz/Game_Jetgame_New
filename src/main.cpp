0480bc69dfe178b144778b950552632ad6435c10
OliverBenz <oliver.benz@outlook.com>
2020-06-21 18:10:50 +0200

    ADDED: images, background done

--- src/main.cpp
+++ src/main.cpp
@@ -11,6 +11,7 @@ void keyboardHandler(SDL_Scancode sc);
 
 // FIXME: Global player, Acces eventHandler
 Player p(0, 0, 64, 64);
+bool endProgram = false;
 
 int main(int argc, char *argv[]){
 	App app;
@@ -19,18 +20,25 @@ int main(int argc, char *argv[]){
 
 	SDL_Event event;
 
-	while(1){
-		SDL_SetRenderDrawColor(app.getRenderer(), 0, 0, 0, 0);
-		SDL_RenderClear(app.getRenderer());
+	// Background Bitmap
+	SDL_Surface *background = SDL_LoadBMP("../res/Background/background.bmp");
+	SDL_Texture *texture = SDL_CreateTextureFromSurface(app.getRenderer(), background);
+
+	while(!endProgram){
+		SDL_RenderCopy(app.getRenderer(), texture, NULL, NULL);
+		//SDL_SetRenderDrawColor(app.getRenderer(), 0, 0, 0, 0);
+		//SDL_RenderClear(app.getRenderer());
 
 		eventHandler(&event);
+
 		p.draw(app.getRenderer());
 
 		SDL_RenderPresent(app.getRenderer());
 
 		//SDL_Delay(200);
 	}
-
+	SDL_DestroyTexture(texture);
+	SDL_FreeSurface(background);
 	app.destroy();
 
 	return 0;
@@ -40,7 +48,7 @@ void eventHandler(SDL_Event *event){
 	while(SDL_PollEvent(event)){
 		switch(event->type){
 			case SDL_QUIT:
-				exit(0);
+				endProgram = true;
 				break;
 			case SDL_KEYDOWN:
 				keyboardHandler(event->key.keysym.scancode);
@@ -54,7 +62,7 @@ void eventHandler(SDL_Event *event){
 void keyboardHandler(SDL_Scancode sc){
 	switch(sc){
 		case SDL_SCANCODE_ESCAPE:
-			exit(0);
+			endProgram = true;
 			break;
 		case SDL_SCANCODE_W:
 			p.move(UP);	
