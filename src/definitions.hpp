e45ebf7af9fca59934876b1e1e7eb8c85d304797
OliverBenz <oliver.benz@outlook.com>
2020-07-12 13:20:01 +0200

    Changed: Player Health

--- src/definitions.hpp
+++ src/definitions.hpp
@@ -5,18 +5,20 @@
 #define SCREEN_WIDTH  1920
 
 // Player
-#define PLAYER_HEALTH 20
+#define PLAYER_HEALTH 5
 #define PLAYER_SPEED 8
 #define PLAYER_WIDTH  160
 #define PLAYER_HEIGHT 90
 
+#define SHOOT_TIMEOUT 100
+
 // Bullet
-#define BULLET_DAMAGE 5
+#define BULLET_DAMAGE 1
 #define BULLET_SPEED 10
 #define BULLET_SIZE 20
 
 // Bitmaps
-#define BM_BACKGROUND "../res/background/background.bmp"
+#define BM_BACKGROUND "../res/background/background_1.bmp"
 #define BM_LEFTWIN	  "../res/background/win_left.bmp"
 #define BM_RIGHTWIN   "../res/background/win_right.bmp"
 
